364b23b97c3b4b1ba0f14cca831528c12dd8a04d
LilyMDev <lilymdev@gmail.com>
2023-07-12 13:27:02 -0500

    Game of RPS

---
+++ Rock Paper Scissors.js
@@ -0,0 +1,19 @@
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+const rps = (p1, p2) => {
+    if(p1 === "scissors") {
+      if(p2 === "rock") return "Player 2 won!"
+      else if(p2 === "paper") return "Player 1 won!"
+      else return "Draw!"
+    }
+    else if(p1 === "paper") {
+      if(p2 === "scissors") return "Player 2 won!"
+      else if(p2 === "rock") return "Player 1 won!"
+      else return "Draw!"
+    }
+    else if(p1 === "rock") {
+      if(p2 === "paper") return "Player 2 won!"
+      else if(p2 === "scissors") return "Player 1 won!"
+      else return "Draw!"
+    }
+  };
